adbdff3613c53fd32d469693d8e43adeca37affa
Mohammad Badrujaman <mohbadrujaman9@gmail.com>
2023-12-03 16:19:05 +0700

    replace route when login

--- lib/screens/login/_login_form.dart
+++ lib/screens/login/_login_form.dart
@@ -35,7 +35,7 @@ class _LoginFormState extends State<LoginForm> {
       const SnackBar(content: Text('Login Berhasil')),
     );
 
-    Get.toNamed(Routes.home);
+    Get.offAllNamed(Routes.home);
   }
 
   @override
